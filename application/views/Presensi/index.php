a251dd22b00e7d0f5b2284e526d1b50c5e8c01e5
MDF <User@NB260151ADV99.intranet.pajak.go.id>
2021-08-24 00:04:34 +0800

    First DEV

--- application/views/Presensi/index.php
+++ application/views/Presensi/index.php
@@ -1,3 +1,14 @@
+<?php
+date_default_timezone_set('Asia/Makassar');
+
+if (date_default_timezone_get()) {
+    echo 'date_default_timezone_set: ' . date_default_timezone_get() . '<br />';
+}
+
+if (ini_get('date.timezone')) {
+    echo 'date.timezone: ' . ini_get('date.timezone');
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>

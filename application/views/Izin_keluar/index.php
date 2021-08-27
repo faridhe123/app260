c4548568cfd58a48ce3d87d3bc1635ffbe9c402d
MDF <User@NB260151ADV99.intranet.pajak.go.id>
2021-08-27 09:17:41 +0800

    LOGOING

--- application/views/Izin_keluar/index.php
+++ application/views/Izin_keluar/index.php
@@ -16,7 +16,9 @@ $fmt->setPattern('cccc, d MMMM yyyy');
                     <p class="card-subtitle card-subtitle-dash"></p>
                     </div>
                     <div>
-                        <button class="btn btn-primary btn-md text-white mb-0 me-0" type="button"><i class="mdi mdi-account-plus"></i>Add new member</button>
+                        <button data-target="#modal-add" data-toggle='modal' class="btn btn-primary btn-md text-white mb-0 me-0" type="button">
+                            <i class="mdi mdi-account-plus"></i>Add new member
+                        </button>
                     </div>
                 </div>
                 <div class="table-responsive">
@@ -66,6 +68,38 @@ $fmt->setPattern('cccc, d MMMM yyyy');
             </div>
         </div>
     </div>
+
+    <!-- Button to Open the Modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+  Open modal
+</button>
+
+<!-- The Modal -->
+<div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Modal Heading</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+        Modal body..
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+
     <script>
       
 

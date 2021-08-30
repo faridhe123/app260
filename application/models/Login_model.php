db2acaef3f2ddaff44e8b3511901f116a020265a
MDF <User@NB260151ADV99.intranet.pajak.go.id>
2021-08-30 18:27:00 +0800

    HTACCESS BISA

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -63,7 +63,7 @@ class Login_model extends CI_model {
 		if (!isset($redirect_to)) 
 		redirect(site_url(''));
 		else
-		redirect(site_url('').substr($redirect_to,8,strlen($redirect_to)));
+		redirect(site_url('').$redirect_to);
 		
 	}
 	else{

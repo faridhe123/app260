e71e40b6b1edbc03d62c9d2716ac885d62fee7b1
as800 <muhamadfajri102@gmail,com>
2022-12-08 15:03:47 +0800

    test lagi

--- application/models/Presensi_model.php
+++ application/models/Presensi_model.php
@@ -95,7 +95,7 @@ class Presensi_model extends CI_model {
 
 		$sql .= "'end' selesai
 				from presensi.v_rekap_presensi a
-					full join u999321655_app260.akun b on a.username = b.username";
+					full join akun b on a.username = b.username";
 					
 		if($bulan !== null) $sql .= " where extract('month' from date_record) = '".$bulan."'";
 		else $sql .= " where date_record between '".$date1."' and '".$date2."'";
